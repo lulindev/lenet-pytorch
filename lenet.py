0049dac5ac682318e13c2d3eda93e51c86042058
Lulin <kistssy+dev@gmail.com>
2020-12-30 20:00:44 +0900

    Add bn

--- lenet.py
+++ lenet.py
@@ -33,14 +33,16 @@ class CustomLeNet(nn.Module):
     def __init__(self):
         super(CustomLeNet, self).__init__()
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.bn1 = nn.BatchNorm2d(10)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.bn2 = nn.BatchNorm2d(20)
         self.fc1 = nn.Linear(20 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = F.max_pool2d(F.relu(self.bn1(self.conv1(x))), 2)
+        x = F.max_pool2d(F.relu(self.bn2(self.conv2(x))), 2)
         x = x.view(-1, 20 * 5 * 5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
