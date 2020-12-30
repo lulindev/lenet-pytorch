ee09657cf8a8351b0c0e269a8ea00699c225fb69
Lulin <kistssy+dev@gmail.com>
2020-12-30 19:31:16 +0900

    Edit fc layers

--- lenet.py
+++ lenet.py
@@ -34,15 +34,17 @@ class CustomLeNet(nn.Module):
         super(CustomLeNet, self).__init__()
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.fc1 = nn.Linear(20 * 5 * 5, 100)
-        self.fc2 = nn.Linear(100, 10)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
         x = F.max_pool2d(F.relu(self.conv1(x)), 2)
         x = F.max_pool2d(F.relu(self.conv2(x)), 2)
         x = x.view(-1, 20 * 5 * 5)
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)  # log_softmax를 수행해야하지만 loss function에 포함되어 있으므로 생략.
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)  # log_softmax를 수행해야하지만 loss function에 포함되어 있으므로 생략.
         return x
 
     def get_name(self):
