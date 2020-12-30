438e9708c9aa1b26fdf3032c27755fb1f8bb168b
Lulin <kistssy+dev@gmail.com>
2020-12-30 18:04:36 +0900

    Change fc layers

--- lenet.py
+++ lenet.py
@@ -31,8 +31,8 @@ class CustomLeNet(nn.Module):
         super(CustomLeNet, self).__init__()
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.fc1 = nn.Linear(20 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
+        self.fc1 = nn.Linear(20 * 5 * 5, 160)
+        self.fc2 = nn.Linear(160, 84)
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
