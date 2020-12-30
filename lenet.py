8449fb41d4fd8f5c35a1978914d2648f090182e4
Lulin <kistssy+dev@gmail.com>
2020-12-30 17:52:07 +0900

    Add CustomLeNet

--- lenet.py
+++ lenet.py
@@ -22,7 +22,26 @@ class LeNet(nn.Module):
         x = x.view(-1, 16 * 5 * 5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = self.fc3(x)  # log_softmax를 수행해야하지만 loss function에 포함됨.
+        x = self.fc3(x)  # log_softmax를 수행해야하지만 loss function에 포함되어 있으므로 생략.
+        return x
+
+
+class CustomLeNet(nn.Module):
+    def __init__(self):
+        super(CustomLeNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.fc1 = nn.Linear(20 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)  # log_softmax를 수행해야하지만 loss function에 포함되어 있으므로 생략.
         return x
 
 
@@ -86,7 +105,7 @@ if __name__ == '__main__':
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
     # 4. Tensorboard
-    writer = torch.utils.tensorboard.SummaryWriter('runs')
+    writer = torch.utils.tensorboard.SummaryWriter('runs/renet')
     writer.add_graph(model, trainloader.__iter__().__next__()[0].to(device))
 
     # 5. Train and test
